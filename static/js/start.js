b1008cdc12bc6b4911daa6c6c880ad4fc2783a31
FOUREX <Foxtrotserega@gmail.com>
2023-12-11 05:09:27 +0200

    Yep

--- static/js/start.js
+++ static/js/start.js
@@ -1,11 +1,29 @@
-let _rotate = document.getElementById("rotate")
-let _scroll = document.getElementById("scroll")
-let _done = document.getElementById("done")
+var _rotate
+var _scroll
+var _scroll_2
+var _done
+var _pizda
+
+function onLoad() {
+    _rotate = document.getElementById("rotate")
+    _scroll = document.getElementById("scroll")
+    _scroll_2 = document.getElementById("scroll-2")
+    _done = document.getElementById("done")
+    _pizda = document.getElementById("pizda")
+
+    setInterval(check, 250)
+}
 
 function check() {
     if (window.matchMedia("(orientation: landscape)").matches) {
+        _rotate.style.display = "none"
+        _scroll.style.display = "flex"
+        _scroll_2.style.display = "flex"
         _done.style.display = "flex"
     } else {
+        _rotate.style.display = "flex"
+        _scroll.style.display = "none"
+        _scroll_2.style.display = "none"
         _done.style.display = "none"
     }
 }
